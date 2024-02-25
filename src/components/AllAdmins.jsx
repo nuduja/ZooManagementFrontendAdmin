e024852489818cf67dfa8e9892d5cb028bc67b1d
nuduja <nudujabandara@gmail.com>
2024-02-25 17:55:02 +0530

    Add All Admin user view

--- src/components/AllAdmins.jsx
+++ src/components/AllAdmins.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Card } from 'primereact/card';
+import React, { useEffect, useState, useMemo } from 'react';
+import { useTable } from 'react-table';
 import { Link } from 'react-router-dom';
 
 const AllAdmins = () => {
@@ -18,22 +18,64 @@ const AllAdmins = () => {
       const data = await response.json();
       setAdmins(data);
     } catch (error) {
-      console.error('Error fetching Admindata:', error);
+      console.error('Error fetching Admin data:', error);
     }
   };
 
+  const data = useMemo(() => admins, [admins]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Username',
+        accessor: 'username',
+      },
+      {
+        Header: 'Actions',
+        Cell: ({ row }) => (
+          <Link to={`/admin/${row.original.username}`} className="p-button p-button-text">
+            View Details
+          </Link>
+        ),
+      },
+    ],
+    []
+  );
+
+  const tableInstance = useTable({ columns, data });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+
   return (
     <div>
-        <h3 className="section-title">Admins</h3>
-        {admins.map(admin => (
-          <Card key={admin.id} title={admin.name} subTitle={`Price: ${admin.username}`} className="ticket-card">
-            <div className="p-mb-2">
-              <Link to={`/admin/${admin.username}`} className="p-button p-button-text">
-                View Details
-              </Link>
-            </div>
-          </Card>
-        ))}
+      <h3 className="section-title">Admins</h3>
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
