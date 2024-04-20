1b678fcf2b074e37caf166ae039dc0a5dc7ada0e
nuduja <nudujabandara@gmail.com>
2024-04-20 09:53:20 +0530

    Update Profile function

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,42 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Card } from 'primereact/card';
-import { Menu } from 'primereact/menu';
 import { Divider } from 'primereact/divider';
 import { Button } from 'primereact/button';
-import { Avatar } from 'primereact/avatar'; // Import Avatar component
+import { Avatar } from 'primereact/avatar';
 import { useNavigate } from 'react-router-dom';
-// import { deleteUser } from '../hooks/deleteUserHook';
 import '../styles/profile.css';
 
 const Profile = () => {
     let navigate = useNavigate();
 
-    // Menu items
-    // const items = [
-    //     { label: 'Profile', icon: 'pi pi-palette', url: '/profile' },
-    //     { label: 'Booked Events', icon: 'pi pi-link', url: '/profile/event' },
-    //     { label: 'Booked Tickets', icon: 'pi pi-home', url: '/ticketprofile' }
-    // ];
-
-    // State for user details
     const [userDetails, setUserDetails] = useState({
         name: '',
         username: '',
         password: ''
     });
 
-    // State for error
     const [error, setError] = useState(null);
 
-    // State for right sidebar
-    // const [showRightSidebar, setShowRightSidebar] = useState(false);
-    // const [rightSidebarContent, setRightSidebarContent] = useState(null);
-
-    // Fetch user data
     useEffect(() => {
-        const fetchData = async (username) => {
+        const fetchData = async (loggedUserId) => {
             try {
-                const response = await fetch(`http://localhost:8080/api/v1/admin/${username}`);
+                const response = await fetch(`http://localhost:8080/api/v1/admin/${loggedUserId}`);
                 const data = await response.json();
                 setUserDetails({
                     name: data.name || '',
@@ -48,8 +32,8 @@ const Profile = () => {
             }
         };
 
-        const loggedUsername = sessionStorage.getItem('loggedUserDetails');
-        fetchData(loggedUsername);
+        const loggedUserId = sessionStorage.getItem('userId');
+        fetchData(loggedUserId);
     }, []);
 
     // Handle delete user
@@ -61,9 +45,6 @@ const Profile = () => {
 
     return (
         <div className="main-container">
-            {/* <div className="left-sidebar">
-                <Menu model={items} />
-            </div> */}
             <div className="profile-section">
                 <Divider />
                 <Card title={<Avatar label={userDetails.name.charAt(0)} style={{ width: '100px', height: '100px', fontSize: '50px'}} />} className="profile-card"> {/* Avatar component */}
@@ -78,13 +59,6 @@ const Profile = () => {
                     <Button label="Delete Profile" icon="pi pi-trash" className="p-button-raised p-button-rounded p-button-danger" onClick={handleDelete} />
                 </div>
             </div>
-            {/* {showRightSidebar && (
-                <div className="right-sidebar">
-                    Booked Tickets details */}
-                    {/* Display rightSidebarContent */}
-                    {/* {rightSidebarContent} */}
-                {/* </div> */}
-            {/* // )} */}
         </div>
     );
 };
